ceea9956a053b07809898d380eb21fbb37e5726c
mohammed-khalif <mohamed.yusuf@student.moringaschool.com>
2023-04-06 15:31:44 -0400

    my thing

---
+++ README.md
@@ -0,0 +1,29 @@
+# The-Champions
+
+## Description 
+This is a web app that showcases the journey Manchester City went through last season to become the champions of england.The website has the following features;
+-A blog that narrates a short story on last season's dramatic events and how they came to win the premier league
+-A video of Manchester united lifting the trophy and celebrating.
+-A feedback form.
+-Players link that contains the squad players of 2013.
+-Awards link that contains the best players that season.
+-Login link that allows you to login to the website.
+-A functioning search bar.
+
+## Requirements
+-Create git hub repository.
+-Add Collaborator(Technical Mentor)
+-Make the neccessary files eg.JS, JSON, HTML & CSS.
+
+
+## Installations
+For this program you do not need to install anything.
+
+## Running the program
+-Run the following URL on the browser to access the program;
+('https://inspiring-chebakia-af3a0b.netlify.app/')
+
+
+## Authors
+
+By mohamed yusuf
--- index.js
+++ index.js
@@ -1,64 +1,64 @@
 function fetchNasa() { 
-    fetch('http://localhost:3001/NASA')
-        .then((res) => res.json())
-        .then((data) => renderNASA(data));    
+  fetch('http://localhost:3000/NASA')
+      .then((res) => res.json())
+      .then((data) => renderNASA(data));    
 }
 
 function renderNASA(data) {
-    const ul = document.getElementById('NASA');
-    ul.innerHTML = "";
+  const ul = document.getElementById('NASA');
+  ul.innerHTML = "";
 
-    data.forEach(pictures => {
-        const li = document.createElement('li');
-        li.classList.add('pointer', 'bold-italic-text');
-        li.innerHTML = pictures.title;
-        ul.appendChild(li);
+  data.forEach(pictures => {
+      //const li = document.createElement('li');
+      //li.classList.add('pointer', 'bold-italic-text');
+    //  li.innerHTML = pictures.title;
+     // ul.appendChild(li);
 
-        const portraitCard = document.createElement("div");
-        portraitCard.classList.add('portrait-card');
-        portraitCard.innerHTML = `
-            <img src="${pictures.poster}" height=500px width=300px/>
-            <h2 class="bold-text">${pictures.title}</h2>
-            <p class="bold-text hidden">${pictures.description}</p>
-            <button class="like-btn">Like</button>
-        `;
+      const portraitCard = document.createElement("div");
+      portraitCard.classList.add('portrait-card');
+      portraitCard.innerHTML = `
+          <img src="${pictures.poster}" height=500px width=300px/>
+          <h2 class="bold-text">${pictures.title}</h2>
+          <p class="bold-text hidden">${pictures.description}</p>
+          <button class="like-btn">Like</button>
+      `;
 
-        const likeBtn = portraitCard.querySelector(".like-btn");
-        let likes = 0;
+      const likeBtn = portraitCard.querySelector(".like-btn");
+      let likes = 0;
 
-        likeBtn.addEventListener("click", function() {
-            likes++;
-            likeBtn.innerHTML = `Liked! (${likes})`;
-            likeBtn.style.backgroundColor = "lightgray";
-            likeBtn.style.cursor = "not-allowed";
-        });
+      likeBtn.addEventListener("click", function() {
+          likes++;
+          likeBtn.innerHTML = `Liked! (${likes})`;
+          likeBtn.style.backgroundColor = "lightgray";
+          likeBtn.style.cursor = "not-allowed";
+      });
 
-        const description = portraitCard.querySelector("p");
-        portraitCard.addEventListener("click", function() {
-            description.classList.toggle("hidden");
-        });
+      const description = portraitCard.querySelector("p");
+      portraitCard.addEventListener("click", function() {
+          description.classList.toggle("hidden");
+      });
 
-        ul.appendChild(portraitCard);
-    });
-    
-    const searchBtn = document.getElementById('searchBtn');
-    searchBtn.addEventListener('click', search);
+      ul.appendChild(portraitCard);
+  });
+  
+  const searchBtn = document.getElementById('searchBtn');
+  searchBtn.addEventListener('click', search);
 
-    function search(event) {
-        event.preventDefault(); // prevent the default form submission behavior
-        const searchInput = document.getElementById('searchInput').value;
+  function search(event) {
+      event.preventDefault(); // prevent the default form submission behavior
+      const searchInput = document.getElementById('searchInput').value;
 
-        // perform the search using the searchInput value
-        // for example, search for articles containing the search term
-        const pictures = document.querySelectorAll('.portrait-card');
-        pictures.forEach(picture => {
-            if (picture.innerText.includes(searchInput)) {
-                picture.style.display = 'block';
-            } else {
-                picture.style.display = 'none';
-            }
-        });
-    }
+      // perform the search using the searchInput value
+      // for example, search for articles containing the search term
+      const pictures = document.querySelectorAll('.portrait-card');
+      pictures.forEach(picture => {
+          if (picture.innerText.includes(searchInput)) {
+              picture.style.display = 'block';
+          } else {
+              picture.style.display = 'none';
+          }
+      });
+  }
 }
 
 fetchNasa();
